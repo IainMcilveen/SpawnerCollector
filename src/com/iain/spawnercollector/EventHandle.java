3e9f24bfbf799e455dd5d40ab970e1d0a1a55e11
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-08-13 22:00:13 -0300

    update (jeremy made it work)

--- src/com/iain/spawnercollector/EventHandle.java
+++ src/com/iain/spawnercollector/EventHandle.java
@@ -1,33 +1,69 @@
 package com.iain.spawnercollector;
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerAnimationEvent;
-import org.bukkit.event.player.PlayerAnimationType;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 public class EventHandle implements Listener {
-	
-	
+
+	private Main main;
+
+	public EventHandle(Main main) {
+		this.main = main;
+	}
+
+	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void startBreaking(PlayerAnimationEvent event) {
-		if(event.getAnimationType() == PlayerAnimationType.ARM_SWING) {
+	public void startBreaking(PlayerInteractEvent event) {
+		if (event.getAction() == Action.LEFT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.SPAWNER) {
+
 			Player player = event.getPlayer();
+			if (player.getInventory().getItemInMainHand().isSimilar(Main.crowbar)) {
+				Block block = event.getClickedBlock();
+				CreatureSpawner spawner = (CreatureSpawner) block.getState();
+				EntityType type = spawner.getSpawnedType();
+				ItemStack item = new ItemStack(Material.SPAWNER);
+				NamespacedKey key = new NamespacedKey(main, "type");
+				ItemMeta meta = item.getItemMeta();
+				meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, type.getName());
+				item.setItemMeta(meta);
+				block.breakNaturally();
+				spawner.getWorld().dropItem(spawner.getLocation(), item);
+			}
 		}
-	
 	}
-	
+
+	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void finishedBreaking(BlockBreakEvent event) {
-		
+	public void onBlockPlaceEvent(BlockPlaceEvent event) {
+		Block block = event.getBlock();
+		if (block.getType() == Material.SPAWNER) {
+
+			ItemStack item = event.getItemInHand();
+			NamespacedKey key = new NamespacedKey(main, "type");
+			EntityType entity = EntityType
+					.fromName(item.getItemMeta().getPersistentDataContainer().get(key, PersistentDataType.STRING));
+			CreatureSpawner state = (CreatureSpawner) block.getState();
+			state.setSpawnedType(entity);
+			state.update();
+		}
 	}
-	
-		
-	
-	//event player animation event, see if the hand moving event holding the crowbar
-	
-	//event for breaking blocks, specifically the spawner and the player holding the crowbar
-	
-	
+
+	// event player animation event, see if the hand moving event holding the
+	// crowbar
+
+	// event for breaking blocks, specifically the spawner and the player holding
+	// the crowbar
+
 }
--- src/com/iain/spawnercollector/Main.java
+++ src/com/iain/spawnercollector/Main.java
@@ -9,25 +9,29 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
+
 	public static final ItemStack crowbar = new ItemStack(Material.GOLDEN_HOE);
-	
+
 	static {
 		ItemMeta crowbarMeta = crowbar.getItemMeta();
 		crowbarMeta.setDisplayName("Crowbar");
-		crowbarMeta.setLore(Arrays.asList("","Can be used to break spawners."));
+		crowbarMeta.setLore(Arrays.asList("", "Can be used to break spawners."));
+		crowbar.setItemMeta(crowbarMeta);
 	}
-	
+
 	@Override
 	public void onEnable() {
-		
-		Bukkit.getPluginManager().registerEvents(new EventHandle(), this);
-		
+
+		Bukkit.getPluginManager().registerEvents(new EventHandle(this), this);
+		Bukkit.getOnlinePlayers().forEach((player) -> {
+			player.getInventory().addItem(crowbar);
+		});
+
 	}
-	
+
 	@Override
 	public void onDisable() {
-		
+
 	}
-	
+
 }
